0c26d0d7ec42325b30688e4ca2fa0af527220bc2
Kind Jeff <shanliangjeff@qq.com>
2016-10-21 10:53:23 +0800

    Delete generate_readme.py

--- generate_readme.py
+++
@@ -1,9 +0,0 @@
-line = '![](img/{}.gif)\r\n'
-content = '新版AC娘表情包\r\n'
-for i in range(1, 56):
-    num = str(i) if i > 9 else '0'+str(i)
-    content += line.format(num)
-
-with open('README.md', 'w', encoding='utf-8') as f:
-    f.write(content)
-print('success')
